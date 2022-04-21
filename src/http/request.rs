def3c218563d30f660922ac4c3c3b251debd1460
Tuan Anh Le <anh.le@myob.com>
2022-04-21 22:17:29 +1000

    feat: using paten matching to get path from query string

--- src/http/request.rs
+++ src/http/request.rs
@@ -19,7 +19,7 @@ impl TryFrom<&[u8]> for Request {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -32,6 +32,27 @@ impl TryFrom<&[u8]> for Request {
 
         let method : Method = method.parse()?;
 
+        let mut query_string = None;
+        // match path.find('?') {
+        //     Some(index) => {
+        //         query_string = Some(&path[i + 1..]);
+        //         path = &path[..i];
+        //     },
+        //     None => {}
+        // }
+
+        // let q = path.find('?');
+        // if q.is_some() {
+        //     let i = q.unwrap();
+        //     query_string = Some(&path[i + 1..]);
+        //     path = &path[..i];
+        // }
+
+        // paten matching
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
 
         unimplemented!()
     }
