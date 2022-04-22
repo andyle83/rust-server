0a259523e03ae71c5746d676f4ea9aa9c3654288
Tuan Anh Le <anh.le@myob.com>
2022-04-22 15:37:03 +1000

    feat: create response

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,7 +1,9 @@
 pub use request::Request;
 pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
 pub mod method;
 pub mod request;
 pub mod query_string;
+pub mod response;
