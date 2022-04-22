6abf444a0d4a4a43f9f81935ae65a2c501d91f0a
Tuan Anh Le <anh.le@myob.com>
2022-04-22 19:44:14 +1000

    feat: implement get and routing via path of request

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,4 +1,4 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Request, Response, StatusCode, Method};
 use crate::http::request::ParseError;
 use super::server::Handler;
 
@@ -6,9 +6,13 @@ pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(
-            StatusCode::Ok,
-            Some("<h1>Hello World</h1>".to_string())
-        )
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None)
+            }
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
